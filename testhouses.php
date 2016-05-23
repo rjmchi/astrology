f229e131f939b39b03270ca0ed0cfd84c507c852
Robert Milanowski <robert@rjmchicago.com>
2016-05-23 12:01:16 -0500

    added test files for houses and planets

---
+++ testhouses.php
@@ -0,0 +1,32 @@
+<?php
+	require_once("cnvt.php");
+	require_once("class.houses.php");
+
+	$mm = 7;
+	$dd=18;
+	$yyyy=1941;
+	$hh=16;
+	$min=3;
+	$tz = 5;
+	$localtime = ($min/60.0)+$hh;
+	$gmt= $localtime+$tz;
+	$lat = 40.033;
+	$long = 76.3;
+	
+
+	$h = new houses($mm,$dd,$yyyy,$gmt, $lat, $long);	
+	$h->PlacHouses();
+	echo "Placidius Houses<br>";
+	printHouses($h->house);
+	
+	$h->KochHouses();
+	echo "<br>Koch Houses<br>";
+	printHouses($h->house);
+	
+function printHouses($h)
+{
+	for ($i=0;$i<12;$i++)
+	{
+		echo $i+1 . ' - '. DecToZod($h[$i]) . '<br>';
+	}
+}
---
+++ testplanets.php
@@ -0,0 +1,22 @@
+<?php
+	require_once("cnvt.php");
+	require_once("class.planets.php");
+
+	$mm = 7;
+	$dd=18;
+	$yyyy=1941;
+	$hh=16;
+	$min=3;
+	$tz = 5;
+	$localtime = ($min/60.0)+$hh;
+	$gmt= $localtime+$tz;
+
+	$p = new planets($mm,$dd,$yyyy,$gmt);	
+	
+	for ($i=0;$i<10;$i++)
+	{
+		echo '<br>';
+		$planet = $p->planets[$i];
+		echo $planet->longName;
+		echo DecToZod($planet->long);
+	}
