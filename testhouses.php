c5ba9c4c9a74d9b7068b43bd373f8174b1dc011c
Robert Milanowski <robert@rjmchicago.com>
2019-12-22 10:36:04 -0600

    Added tests for other house systems

--- testhouses.php
+++ testhouses.php
@@ -22,6 +22,32 @@
 	$h->KochHouses();
 	echo "<br>Koch Houses<br>";
 	printHouses($h->house);
+
+	$h->RegiomontanusHouses();
+	echo "<br>Regiomontanus Houses<br>";
+	printHouses($h->house);
+
+	$h->PorphyryHouses();
+	echo "<br>Porphyry Houses<br>";
+	printHouses($h->house);
+
+	$h->MeridianHouses();
+	echo "<br>Meridian Houses<br>";
+	printHouses($h->house);
+
+	$h->MorinusHouses();
+	echo "<br>Morinus Houses<br>";
+	printHouses($h->house);
+
+	$h->CampanusHouses();
+	echo "<br>Campanus Houses<br>";
+	printHouses($h->house);
+
+	$h->TopocentricHouses();
+	echo "<br>Topocentric Houses<br>";
+	printHouses($h->house);
+	
+	
 	
 function printHouses($h)
 {
